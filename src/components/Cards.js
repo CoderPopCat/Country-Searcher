f3b64554ae24d36014d3a849f2e7056a17d4f87f
CoderPopCat <rguy6085@gmail.com>
2023-08-14 11:29:30 +0400

    Responsiveness

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -95,12 +95,12 @@ const Cards = ({ }) => {
                     <label for="default-search" className="mb-2 text-md font-medium text-white">Search</label>
                     <div className="relative">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <svg className="lg:block hidden w-4 h-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="search" id="default-search" className="block w-[50%] lg:w-[46%] p-4 pl-10 text-sm bg-[#021d21] border-gray-600 placeholder-gray-400 text-whitee focus:outline-none rounded-lg" placeholder="Search For A Country..." onChange={(e) => e.target.value.length === 0 ? setCountries(Info) : setCountries(countries.filter(c => c.country.toLowerCase().includes(e.target.value.toLowerCase())))} required />
-                        <button onClick={() => setOpen(true)} type="button" className="flex justify-center items-center gap-2 absolute right-0 top-0 text-white bg-[#ffffff0d] focus:ring-4 focus:ring-[#021d21] focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 w-[255px] pl-3 text-center mr-2 mb-2" style={{
+                        <input type="search" id="default-search" className="block w-[100%] lg:w-[46%] p-4 pl-10 text-sm bg-[#021d21] border-gray-600 placeholder-gray-400 text-whitee focus:outline-none rounded-lg" placeholder="Search For A Country..." onChange={(e) => e.target.value.length === 0 ? setCountries(Info) : setCountries(countries.filter(c => c.country.toLowerCase().includes(e.target.value.toLowerCase())))} required />
+                        <button onClick={() => setOpen(true)} type="button" className="flex justify-center items-center gap-2 mt-5 lg:mt-0 lg:absolute right-0 top-0 text-white bg-[#ffffff0d] focus:ring-4 focus:ring-[#021d21] focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 w-[255px] pl-3 text-center mr-2 mb-2" style={{
                             paddingLeft: '2.5rem',
                             padding: '0.8rem',
                             fontSize: '21px',
